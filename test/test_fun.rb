b950907e6612d05f9aa2f4c49c69349e576fea5b
Alecs <41908942+ThatAlecs@users.noreply.github.com>
2023-10-15 16:38:37 +0200

    Update test_fun.rb

--- test/test_fun.rb
+++ test/test_fun.rb
@@ -1,4 +1,4 @@
-# test_customization.rb
+# test_fun.rb
 
 $LOAD_PATH << File.expand_path('../lib', __dir__)
 require 'customization'
@@ -21,8 +21,15 @@ end
 puts "\nText Borders:"
 Customization::BORDERS.keys.each do |border|
   text = "This has a #{border.to_s} border."
+  
+  # Test default border
   puts Customization.set_text_border(text, border)
+  puts Customization.end
+  
+  # Test border with custom color and thickness
+  puts Customization.set_text_border(text, border, color: :green, thickness: 2)
 end
+puts Customization.end
 
 # Test text alignment
 puts "\nText Alignment:"
@@ -53,6 +60,14 @@ puts "\nBlinking Text:"
 blinking_text = Customization.apply_blinking_effect("This text is blinking.")
 puts blinking_text
 
+puts Customization.space
+
+puts "Some text."
+puts Customization.space  # This line adds a blank line.
+puts "More text."
+
+puts Customization.space
+
 # Test gradient text
 puts "\nGradient Text:"
 gradient_colors = [:red, :orange, :yellow, :green, :blue, :purple]
@@ -64,21 +79,39 @@ puts "\nText with Shadow and Blur:"
 shadow_color = { r: 64, g: 64, b: 64 } # Customize the shadow color (e.g., a darker gray)
 text_with_shadow_and_blur = Customization.text_with_shadow("Text with shadow and blur", shadow_color, 5, 3, 2)
 puts text_with_shadow_and_blur
+Customization.space 
+Customization.end
 
 wrapped_text = Customization.wrap_text("This is a long text that needs wrapping.", 20)
 puts wrapped_text
+puts Customization.space
+
+# Uppercase text
+original_text = "Hello, World!"
+uppercase_text = Customization.upcase(original_text)
+puts uppercase_text
+
+# Lowercase Text
+original_text = "Hello, World!"
+lowercase_text = Customization.downcase(original_text)
+puts lowercase_text
+Customization.space = 2 
 
 # Using a custom color
 custom_color = "\e[38;5;200m"  # Custom ANSI code for pink color
 puts Customization.color_text("This is a custom color", nil, custom_color)
 
-# Test text animation
-puts "\nText Animation:"
+# Test animated text
+puts "\nAnimated Text:"
 
-# Test scrolling text
-scrolling_text = "This text is scrolling from right to left."
-Customization.scroll_text(scrolling_text, 10) # You can adjust the speed (e.g., 10 characters per second)
+# Test scrolling text animation
+scrolling_text = "This text is scrolling with animation."
+Customization.animate_text(scrolling_text, speed: 10, delay: 0.1)
 
-# Test typewriter text
-typewriter_text = "This text is being typed like a typewriter."
-Customization.typewriter_text(typewriter_text, 0.1) # You can adjust the speed (e.g., 0.1 seconds per character)
+# Test typewriter text animation
+typewriter_text = "This text is being typed with animation."
+Customization.animate_text(typewriter_text, speed: 0.1, delay: 0.2)
+Customization.space
+Customization.stop_animations
+sleep(1)
+Customization.animation_end
